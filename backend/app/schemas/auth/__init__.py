29b4b10f8481d8cec1d1e9785ae607d46775b660
ShviXXL <shvix@pm.me>
2021-02-18 00:40:11 +0300

    Extract auth shemas in separate package

---
+++ backend/app/schemas/auth/__init__.py
@@ -0,0 +1,3 @@
+"""Auth schemas."""
+
+from .user import UserIn, UserOut, UserDB
--- backend/app/schemas/auth/user.py
+++ backend/app/schemas/auth/user.py
@@ -1,28 +1,28 @@
-"""Schemas for authentication"""
+"""User schemas."""
 
 from typing import Optional
 
 from pydantic import BaseModel
 from pydantic import EmailStr
 
-from .base import MongoModel
+from ..base import MongoModel
 
 
 class User(MongoModel):
-    """Base User scheme"""
+    """Base User scheme."""
     email: Optional[EmailStr] = None
 
 
 class UserIn(BaseModel):
-    """Input User scheme"""
+    """Input User scheme."""
     email: EmailStr
     password: str
 
 
 class UserOut(User):
-    """Output User scheme"""
+    """Output User scheme."""
 
 
 class UserDB(User):
-    """Database User scheme"""
+    """Database User scheme."""
     password_hash: str
