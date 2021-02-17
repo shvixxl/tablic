da2cbe410ecfaa1a85655b99eaeb25f2f46d41b1
ShviXXL <shvix@pm.me>
2021-02-18 00:25:03 +0300

    Add token helpers

---
+++ backend/app/helpers/auth/token.py
@@ -0,0 +1,32 @@
+"""Token helpers."""
+
+from typing import Any
+from datetime import datetime, timedelta
+
+import jwt
+
+ALGORITHM = 'HS256'
+
+
+async def generate_token(
+    subject: Any,
+    expires: timedelta,
+    secret: str,
+    algorithm: str = ALGORITHM,
+) -> str:
+    """Generates JWT token."""
+    exp = datetime.utcnow() + expires
+    payload = {'exp': exp, 'sub': str(subject)}
+    return jwt.encode(payload, secret, algorithm)
+
+
+async def verify_token(
+    token: str,
+    secret: str,
+    algorithm: str = ALGORITHM,
+) -> str:
+    """Verifies JWT token."""
+    try:
+        return jwt.decode(token, secret, algorithm).get('sub')
+    except jwt.PyJWTError:
+        return None
